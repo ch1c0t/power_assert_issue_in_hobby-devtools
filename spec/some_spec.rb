a2c6106ae1338650dd620adb2abcf555f21a5f21
Anatoly Chernow <chertoly@gmail.com>
2017-06-09 21:47:45 +0300

    Power Assert no longer shows a helpful output on 'bundle exec rspec'

--- spec/some_spec.rb
+++ spec/some_spec.rb
@@ -3,6 +3,8 @@ require 'rspec/power_assert'
 RSpec::PowerAssert.example_assertion_alias :assert
 RSpec::PowerAssert.example_group_assertion_alias :assert
 
+require 'hobby/devtools/rspec_helper'
+
 def one
   1
 end
